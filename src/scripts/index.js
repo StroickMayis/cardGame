f62393f627b05a7822b5eae512a911caa19c0679
Erik Mueller <erik_o_mueller@yahoo.com>
2025-04-09 16:57:58 -0500

    feat: init setup for the project, architectural

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,5 @@
 import "../index.css"; 
+
+// I am going to make a central command object that controls and communicates with all of the other units. 
+// Central command will be in charge of delivering state to its functional units to manipulate or work with.
+
